6e221a68edcd8e011e5fb56e0f6397020e918c74
Anwesh43 <anweshthecool0@gmail.com>
2021-03-23 01:55:10 +0530

    adding constants and libraries

--- barballfromedgeview/src/main/java/com/example/barballfromedgeview/BarBallFromEdgeView.kt
+++ barballfromedgeview/src/main/java/com/example/barballfromedgeview/BarBallFromEdgeView.kt
@@ -1,2 +1,25 @@
 package com.example.barballfromedgeview
 
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#3F51B5",
+    "#1A237E",
+    "#F57F17",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val barFactor : Float = 9.8f
+val circleFactor : Float = 26.2f
+val delay : Long = 20
+val scGap : Float = 0.02f / parts
+val backColor : Int = Color.parseColor("#BDBDBD")
