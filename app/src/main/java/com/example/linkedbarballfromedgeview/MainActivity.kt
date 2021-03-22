2d01ad1bbb99aecf2fbbd02b9c10bbb7ab81c708
Anwesh43 <anweshthecool0@gmail.com>
2021-03-23 02:29:17 +0530

    adding view in activity

--- app/src/main/java/com/example/linkedbarballfromedgeview/MainActivity.kt
+++ app/src/main/java/com/example/linkedbarballfromedgeview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedbarballfromedgeview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.barballfromedgeview.BarBallFromEdgeView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BarBallFromEdgeView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
